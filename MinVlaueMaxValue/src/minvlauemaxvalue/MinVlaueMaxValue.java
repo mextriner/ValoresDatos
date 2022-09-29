cf7a24c69039d54d8dc686bf648a5fa72a26897b
mextriner <mextrinerx1998@gmail.com>
2022-09-29 09:08:22 +0200

    File MinMax addeed
    The main project was addeed

---
+++ MinVlaueMaxValue/src/minvlauemaxvalue/MinVlaueMaxValue.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minvlauemaxvalue;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class MinVlaueMaxValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ENTEROS:\n");
+        System.out.println("El valor máximo del Byte es "+ Byte.MAX_VALUE+" y "
+                + "el mínimo es "+Byte.MIN_VALUE);
+       
+        System.out.println("El valor máximo del int es "+Integer.MAX_VALUE+" y el "
+                + "mínimo es "+Integer.MIN_VALUE);
+      
+        System.out.println("El valor máximo del short es "+Short.MAX_VALUE+" "
+                + "y el mínimo es "+Short.MIN_VALUE);
+        
+        System.out.println("El valor máximo del long es "+ Long.MAX_VALUE+" "
+                + "y el mínimo es "+ Long.MIN_VALUE);
+        
+        System.out.println("\nDECIMALES:\n");
+        System.out.println("El valor máximo del Float es "+ Float.MAX_VALUE  +" y "
+                + "el mínimo es "+Float.MIN_VALUE);
+       
+        System.out.println("El valor máximo del Double es "+Double.MAX_VALUE+" y el "
+                + "mínimo es "+Double.MIN_VALUE);
+        
+        System.out.println("\nCARACTERES Y CADENAS:\n");
+        System.out.println("El valor máximo del Char es "+ Character.MAX_VALUE+" y "
+                + "el mínimo es "+Character.MIN_VALUE);
+       
+        System.out.println("El valor máximo del Double es "+Double.MAX_VALUE+" y el "
+                + "mínimo es "+Double.MIN_VALUE);
+      
+    }
+    
+}
